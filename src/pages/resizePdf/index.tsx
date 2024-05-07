c00458b21c649bcc04f2efc8da0ddfb2b3030767
mohd anas <localmail.office@gmail.com>
2024-05-07 11:16:00 +0530

    update - resize pdf

--- src/pages/resizePdf/index.tsx
+++ src/pages/resizePdf/index.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadFile from "../../components/UploadFile";
 import Button from "../../components/Button";
 
 const Index = () => {
+  const [file, setFile] = useState<File>();
   const uploadPDF = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files;
     if (file != null && file.length > 0) {
-      console.log("we can resize the pdf file.");
+      if (file[0].type === "application/pdf") {
+        setFile(file[0]);
+      } else {
+        alert("please select only pdf files.");
+      }
     }
   };
   return (
     <div className=" w-full h-full flex items-center justify-center">
       <div className="flex flex-col gap-y-5">
         <UploadFile onChangeHandle={uploadPDF} />
-        <Button disable={true}>Resize PDF</Button>
+        <Button
+          disable={file ? false : true}
+          onClickHandler={() => console.log(file)}
+        >
+          Resize PDF
+        </Button>
       </div>
     </div>
   );
