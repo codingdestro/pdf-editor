f37cf8190249d8fc0c3bcff5fc432b5c87b1bdd8
mohd anas <localmail.office@gmail.com>
2024-05-11 11:12:51 +0530

    fix add - add Dockerfile and fix resizePdf and delete App.tsx

--- src/pages/resizePdf/index.tsx
+++ src/pages/resizePdf/index.tsx
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import UploadFile from "../../components/UploadFile";
 import Button from "../../components/Button";
 
 const Index = () => {
   const [file, setFile] = useState<File>();
-  const uploadPDF = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files;
-    if (file != null && file.length > 0) {
-      if (file[0].type === "application/pdf") {
-        setFile(file[0]);
-      } else {
-        alert("please select only pdf files.");
-      }
+  const uploadPDF = (e: File) => {
+    if (e.type === "application/pdf") {
+      setFile(e);
+    } else {
+      alert("please select only pdf files.");
     }
   };
+
   return (
     <div className=" w-full h-full flex items-center justify-center">
       <div className="flex flex-col gap-y-5">
-        <UploadFile onChangeHandle={uploadPDF} />
+        <UploadFile onChangeHandle={(e) => uploadPDF(e)} />
         <Button
           disable={file ? false : true}
           onClickHandler={() => console.log(file)}
